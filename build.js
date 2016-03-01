c9642bfff484d736f31a217695713a370c6d5925
dreyguy <dreyguy@gmail.com>
2016-02-29 18:44:49 -0600

    Adding optional Travis build number to build
    Build server will now populate build number in version of extension.

--- build.js
+++ build.js
@@ -1,18 +1,36 @@
 var fs = require('fs');
-var archiver = require('archiver');
 
-var checkDir = fs.existsSync('build') || fs.mkdirSync('build');
+bump();
+createZip();
 
-var output = fs.createWriteStream('build/rallyrap.zip');
-var archive = archiver('zip');
-output.on('close', function() {
-  console.log('ZIP Created - build/rallyrap.zip');
-  console.log('Wrote ' + archive.pointer() + 'bytes');
-});
-archive.on('error', function(err) {
-  throw err;
-});
-archive.pipe(output);
-archive.glob('!(build|node_modules)*');
-archive.glob('!(build|node_modules)/**/*');
-archive.finalize();
+function bump() {
+     if (process.argv[2]) {
+        var manifestPath = './manifest.json';
+        var manifest = require(manifestPath);
+        var version = manifest.version.split('.');
+
+        manifest.version = version[0] + '.' + version[1] + '.' + process.argv[2];
+        console.log('Bumping version to ' + manifest.version);
+        fs.writeFileSync(manifestPath, JSON.stringify(manifest, null, 4));
+     }
+}
+
+function createZip() {
+    var archiver = require('archiver');
+
+    var checkDir = fs.existsSync('build') || fs.mkdirSync('build');
+
+    var output = fs.createWriteStream('build/rallyrap.zip');
+    var archive = archiver('zip');
+    output.on('close', function() {
+      console.log('ZIP Created - build/rallyrap.zip');
+      console.log('Wrote ' + archive.pointer() + 'bytes');
+    });
+    archive.on('error', function(err) {
+      throw err;
+    });
+    archive.pipe(output);
+    archive.glob('!(build|node_modules)*');
+    archive.glob('!(build|node_modules)/**/*');
+    archive.finalize();
+}
