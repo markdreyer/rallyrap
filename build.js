39fdc1f736f5709344e6426de4699da7c805258a
dreyguy <dreyguy@gmail.com>
2016-03-19 12:29:50 -0500

    Fixed build issue with local build not creating zip

--- build.js
+++ build.js
@@ -1,29 +1,35 @@
 var fs = require('fs');
+var isCiMasterBuild = process.env.TRAVIS_BRANCH === 'master' && process.env.TRAVIS_PULL_REQUEST === 'false';
+var isLocalBuild = process.env.TRAVIS_BRANCH === undefined;
 
 console.log('PR was:' + process.env.TRAVIS_PULL_REQUEST);
 console.log('Branch was:' + process.env.TRAVIS_BRANCH);
 
-if (process.env.TRAVIS_BRANCH === 'master' && process.env.TRAVIS_PULL_REQUEST === 'false') {
-    console.log('****Publishing Extension****');
+if (isCiMasterBuild) {
+    console.log('****MASTER Branch Build****');
     bump();
+    createZip();
+} else if (isLocalBuild) {
     createZip();
 } else {
     console.log('****SKIP Publish****');
 }
 
 function bump() {
+    console.log('****Bump Version****');
      if (process.argv[2]) {
         var manifestPath = './manifest.json';
         var manifest = require(manifestPath);
         var version = manifest.version.split('.');
 
-        manifest.version = version[0] + '.' + version[1] + '.' + process.argv[2];
+        manifest.version = version[0] + '.' + version[1] + '.' + process.argv[2];//$TRAVIS_BUILD_NUMBER
         console.log('Bumping version to ' + manifest.version);
         fs.writeFileSync(manifestPath, JSON.stringify(manifest, null, 4));
      }
 }
 
 function createZip() {
+    console.log('****Create ZIP****');
     var archiver = require('archiver');
 
     var checkDir = fs.existsSync('build') || fs.mkdirSync('build');
