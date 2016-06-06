34606a468f75ab41f4eb63205ced4682dc5d636c
Parks <HParks@rwbaird.com>
2016-06-06 16:04:50 -0500

    Found left-click call,added possible sound option

--- js/inject.js
+++ js/inject.js
@@ -2,11 +2,10 @@ var background = {
     init: function() {
         chrome.browserAction.onClicked.addListener(function(tab) {
           chrome.tabs.executeScript({
-            code: 'raptorize()'
+            code: 'raptorize()' //Left-click action
           });
         });
     }
 };
 
 background.init();
-
